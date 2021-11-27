2e227fe695a6ada8f576c98199539c6e7a89339f
indranisen2003 <46016997+indranisen2003@users.noreply.github.com>
2021-11-27 23:23:22 +0530

    Update test1.R

--- test1.R
+++ test1.R
@@ -1,1 +1,25 @@
+df  = read.csv('IPL.csv',header= TRUE )
+names(df)
+class(df)
+
+dim(df)
+summary(df)
+library(dplyr)
+######runs#####
+runs  = df %>%
+  group_by(batsman,id)%>%
+  summarize("score" = sum(batsman_runs))
+runs
+unique(runs$batsman)
+dim(runs)
+runs$round = substr(runs$id, start = 1, stop = 3)
+
+str(runs$round)
+unique(runs$round)
+####wickets#####
+wickets  = df %>%
+  group_by(bowler, id)%>%
+  summarize(wicket = sum(is_wicket))
+wickets
+unique(wickets$bowler)
 
